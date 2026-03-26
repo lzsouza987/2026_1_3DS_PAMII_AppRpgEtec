9060be29e6317fda7836df44ecd317ea6a489d92
lzsouza987 <luizfernando987@gmail.com>
2026-03-25 22:28:44 -0300

    Add project files.

---
+++ AppRpgEtec/Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,37 @@
+﻿using AppRpgEtec.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppRpgEtec.Services.Usuarios
+{
+    public class UsuarioService : Request
+    {
+        private readonly Request _request;
+        private const string _apiUrlBase = "http://xyz.somee.com/RpgApi/Usuarios";
+        //private const string _apiUrlBase = "https://xyz.azurewebsites.net/Usuarios";
+
+        public UsuarioService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<Usuario> PostRegistrarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Registrar";
+            u.Id = await _request.PostReturnIntAsync(_apiUrlBase + urlComplementar, u, string.Empty);
+
+            return u;
+        }
+
+        public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Autenticar";
+            u = await _request.PostAsync(_apiUrlBase + urlComplementar, u, string.Empty);
+
+            return u;
+        }
+
+
+    }
+}
---
+++ AppRpgEtec/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AppRpgEtec.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke
+                (this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
